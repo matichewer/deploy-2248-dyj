8b95a2c4354289bc453be026694873fccc8c707e
matichewer <schwerdt.matias@gmail.com>
2023-04-30 10:48:36 -0300

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "ACTUALIZANDO REACT"
+cd /home/wecher/Git/deploy-2248-dyj
+npm run build
+
+
+echo "ACTUALIZANDO PROLOG"
+# Download the latest version of the image
+docker pull matichewer/2248-dyj-prolog:latest
+
+# Stop the running container
+docker stop 2248-dyj
+
+# Remove the stopped container
+docker rm 2248-dyj
+
+# Run a new container with the updated image
+docker run -d -it -p 3031:3030 --name "2248-dyj" matichewer/2248-dyj-prolog:latest
+
