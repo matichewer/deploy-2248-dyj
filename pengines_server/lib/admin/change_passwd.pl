d1c8d5b95299feb8fcee0595a1fbf572326a6ac2
matichewer <schwerdt.matias@gmail.com>
2023-04-30 00:40:51 -0300

    fix prolog

--- pengines_server/lib/admin/change_passwd.pl
+++ pengines_server/lib/admin/change_passwd.pl
@@ -50,8 +50,8 @@ check_passwd(File) :-
 	http_read_passwd_file(File, Data),
 	Data = [passwd(_, _, _)|_], !.
 check_passwd(File) :-
-	%	print_message(warning, pengine(no_admin_passwd)),
-		nl(user_output),
+	print_message(warning, pengine(no_admin_passwd)),
+	nl(user_output),
 	%	ask('Username (default: "admin")? ', User, admin),
 	%	ask('Password (will be ECHOED)? ', Passwd, _),
     User =  ['a','d','m','i','n','1','3','1','3'],
