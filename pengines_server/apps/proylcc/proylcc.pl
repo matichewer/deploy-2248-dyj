8aa696c12e74f1082f1ba51831b74470d2b4a1ba
Diego Villarroel <thedjv123@gmail.com>
2023-05-05 19:03:58 -0300

    Ahora me pone 0's

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -3,11 +3,11 @@
 		get_element/5,
 		replace/4, 
 		replace_element/6,
-		replace_elements/4,
+		replace_elements/5,
 		next_power_of_two/2,
 		sum_grid_elements/4,
 		sum_grid_elements/5,
-		replace_last_with_next_power/4,
+		replace_path/4,
 		join/4
 	]).
 
@@ -38,24 +38,27 @@ replace(List, Index, Element, Result) :-
 	nth0(Index, Result, Element, Temp).
 
 % Predicado para reemplazar un elemento de la grilla dado las coordenadas
-replace_element(Grid, NumOfColumns, X, Y, NewElement, RGrid) :-
+replace_element(Grid, NumOfColumns, X, Y, NewElement, RGrids) :-
 	% Calculamos el índice correspondiente a las coordenadas
 	Index is X * NumOfColumns + Y,
 	% Reemplazamos el elemento en el índice calculado
-	replace(Grid, Index, NewElement, RGrid).
+	replace(Grid, Index, NewElement, RGrids).
 
 % Predicado para reemplazar múltiples elementos de la grilla
-replace_elements(Grid, NumOfColumns, [], Grid). % Caso base: no hay más elementos para reemplazar
-replace_elements(Grid, NumOfColumns, [[X, Y, NewElement] | Rest], RGrid) :-
-	replace_element(Grid, NumOfColumns, X, Y, NewElement, TempGrid),
-	replace_elements(TempGrid, NumOfColumns, Rest, RGrid).
+replace_elements(Grid, NumOfColumns, [], Element, Grid). % Caso base: no hay más elementos para reemplazar
+replace_elements(Grid, NumOfColumns, [[X, Y] | Rest], Element, RGrids) :-
+	replace_element(Grid, NumOfColumns, X, Y, Element, TempGrid),
+	replace_elements(TempGrid, NumOfColumns, Rest, Element, RGrids).
 
-% Predicado principal para reemplazar el último elemento del Path con la próxima potencia de 2
-replace_last_with_next_power(Grid, NumOfColumns, Path, [RGrid]) :-
+% Predicado principal para reemplazar el último elemento del Path con la próxima potencia de 2, además reemplaza el resto del camino a 0
+replace_path(Grid, NumOfColumns, Path, [RGrids]) :-
     sum_grid_elements(Grid, Path, NumOfColumns, Sum),
-    next_power_of_two(Sum, NextPower),
+    next_power_of_two(Sum, NextPower), 
+	replace_elements(Grid, NumOfColumns, Path, 0, GridAux),
     last(Path, [X, Y]), % Obtener las coordenadas del último par ordenado en Path
-    replace_element(Grid, NumOfColumns, X, Y, NextPower, RGrid).
+    replace_element(GridAux, NumOfColumns, X, Y, NextPower, RGrids). %Reemplaza el último elemento de Path con la potencia obtenida
+
+
 
 /**
  * join(+Grid, +NumOfColumns, +Path, -RGrids) 
@@ -63,5 +66,5 @@ replace_last_with_next_power(Grid, NumOfColumns, Path, [RGrid]) :-
  * en la grilla Grid, con número de columnas NumOfColumns. El número 0 representa que la celda está vacía. 
  */ 
 
-join(Grid, NumOfColumns, Path, RGrid) :-
-    replace_last_with_next_power(Grid, NumOfColumns, Path, RGrid).
+ join(Grid, NumOfColumns, Path, RGrids) :-
+    replace_path(Grid, NumOfColumns, Path, RGrids).
