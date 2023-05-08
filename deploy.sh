543e74da7089e65a4d13ef6b06e57d3074d9c7fe
matichewer <schwerdt.matias@gmail.com>
2023-05-08 15:34:42 -0300

    Fix deploy npm error

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/bin/sh
+
+# Para que funcione sin problemas NPM
+#export NVM_DIR="/home/wecher/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 
 echo "ACTUALIZANDO REACT"
 cd /home/wecher/Git/deploy-2248-dyj
 git pull
-/usr/local/bin/npm run build
+npm run build
 
 
 echo "ACTUALIZANDO PROLOG"
